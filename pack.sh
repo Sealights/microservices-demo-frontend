5869552c8d404521ab0d2a9447e9db08bfec6b20
pp <piotr.pernej@sealights.io>
2022-06-13 10:45:08 +0200

    Dummy change

--- pack.sh
+++ pack.sh
@@ -1,3 +1,4 @@
 docker build -t frontend .
 docker tag frontend:latest 159616352881.dkr.ecr.eu-west-1.amazonaws.com/microservices-demo-frontend:latest
 docker push 159616352881.dkr.ecr.eu-west-1.amazonaws.com/microservices-demo-frontend:latest
+
